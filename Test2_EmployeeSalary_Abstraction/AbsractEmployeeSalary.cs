6f38ce5ff028fee13e0c55be3a9e7238445f1ab0
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-11-12 14:23:26 +0530

    Add project files.

---
+++ Test2_EmployeeSalary_Abstraction/AbsractEmployeeSalary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2_EmployeeSalary_Abstraction
+{
+    abstract class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string? EmployeeName { get; set; }
+        public int EmployeeSalary { get; set; }
+
+
+
+        public abstract void DisplayEmployeeDetails();
+
+
+
+        public abstract void TaxCalculation();
+        public abstract void DisplaySalaryDetails();
+
+
+
+    }
+}
---
+++ Test2_EmployeeSalary_Abstraction/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Test2_EmployeeSalary_Abstraction;
+
+
+
+Salary objSalary = new Salary();
+Console.WriteLine("Enter Employee Details.....");
+Console.WriteLine("Enter Employee Id");
+objSalary.EmployeeId = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Employee Name");
+objSalary.EmployeeName = Console.ReadLine();
+
+Console.WriteLine("Enter Employee Salary");
+objSalary.EmployeeSalary = int.Parse(Console.ReadLine());
+
+objSalary.DisplayEmployeeDetails();
+objSalary.TaxCalculation();
+objSalary.DisplaySalaryDetails();
+
+
+
+   
+
